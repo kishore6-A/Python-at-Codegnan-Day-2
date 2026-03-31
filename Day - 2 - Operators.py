b288bb09108b41e70d782fedcf5b1cde4dc5b346
JANGA KISHORE <jangakishore06@gmail.com>
2026-03-31 17:19:49 +0530

    Add files via upload

---
+++ Day - 2 - Operators.py
@@ -0,0 +1,46 @@
+a=36
+b=52
+c=a+b
+print("Addition of Two Numbers:", c)
+#
+#
+#
+first_number=254
+second_number=160
+result=first_number-second_number
+print("Subtraction of Two Numbers:", result)
+#
+#
+#
+A=71
+B=99
+C=A*B
+print("Multiplication of Two Numbers:", C)
+#
+#
+#
+First_Number=321
+Second_Number=123
+Result=First_Number/Second_Number
+print("Division of Two Numbers:", Result)
+#
+#
+#
+Hi=456
+Hello=146
+Total=Hi%Hello
+print("Modulus of Two Numbers:", Total)
+#
+#
+#
+Offline=725
+Online=382
+Final=Offline//Online
+print("Floor Division of Two NUmbers:", Final)
+#
+#
+#
+End_1=981
+End_2=36
+Finally=End_1**End_2
+print("Exponentiation of Two Numbers:", Finally)
