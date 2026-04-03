1c31fe7a6c42975f38e1ac83f8ff1459dfb1cd28
JANGA KISHORE <jangakishore06@gmail.com>
2026-04-03 07:23:07 +0530

    Add files via upload

---
+++ Day - 4.py
@@ -0,0 +1,61 @@
+num=int(input())
+if num==0:
+    print("Number is Zero")
+else:
+    print("Number is Non-zero")
+
+####
+num=int(input())
+if num>0:
+    print("Number is Positive")
+elif num<0:
+    print("Number is Negative")
+else:
+    print("Number is Zero")
+
+###
+
+num=int(input())
+if num%2==0:
+    print("Number is Even")
+else:
+    print("Number is Odd")
+
+###
+age=int(input())
+if age>=18:
+    print("vote is Eligible")
+else:
+    print(" Vote is Not Eligible")
+
+###
+num=int(input())
+if num%3==0:
+    print("Divisible by 3")
+else:
+    print("Not Divisible by 3")
+
+###
+num=int(input())
+if num%3==0 and num%5==0:
+    print("Divisible by both 3 and 5")
+else:
+    print("Not Divisible by both 3 and 5")
+
+
+###
+num1=int(input())
+num2=int(input())
+if num1>num2:
+    print("first number is Bigger")
+elif num2>num1:
+    print("second number is Bigger")
+else:
+    print("Both Numbers are Equals")
+ 
+    
+
+
+
+
+    
