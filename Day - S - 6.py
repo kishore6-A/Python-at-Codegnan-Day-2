d178fd9e262c842105620e78698e860eeee1629c
JANGA KISHORE <jangakishore06@gmail.com>
2026-04-05 22:54:52 +0530

    Add files via upload

---
+++ Day - S - 6.py
@@ -0,0 +1,11 @@
+#take three largest numbers using comprasion operator
+
+a=int(input())
+b=int(input())
+c=int(input())
+if a>b:
+    print("a is greater than")
+elif b>c:
+    print("b is greater than")
+else:
+    print("c is greater than")
